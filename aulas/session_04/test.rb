4d641801c31e1b098f3a5ac49b009b512e7c6487
George Chaves <george_hfc@yahoo.com.br>
2023-04-20 17:51:37 +0100

    intro ruby, bebedouro images

--- aulas/session_04/test.rb
+++ aulas/session_04/test.rb
@@ -1,1 +1,33 @@
-puts "Hello Ruby!"
+# IRB = Interactive Ruby
+puts "### Hello Ruby! ###"
+# puts true.class
+# puts false.class
+
+puts 'Qual o seu nome?'
+resp = gets.chomp
+puts 'E a sua idade?'
+idade = gets.chomp.to_i
+puts "Olá, #{resp}! Seu nome tem #{resp.length} caracteres"
+resp.length.even? ? (puts "#{resp.length} é par, incrível") : (puts "#{resp.length} é ímpar, incrível")
+if idade >= 18
+  puts 'Podes votar'
+else
+  puts 'Te vejo ano que vem!'
+end
+
+# v = ["Ana", "João", "Pedro"]
+# puts v.size
+# puts v.sort
+
+username = 'george'
+password = '1234'
+puts 'Por favor insira o seu nome de utilizador:'
+resp1 = gets.chomp
+puts 'Por favor insira a sua password:'
+resp2 = gets.chomp
+
+if resp1 == username && resp2 == password
+  puts 'Olá, vem bindo!'
+else
+  puts 'Credenciais erradas, por favor tente novamente'
+end
