8806a6a79af2fefd9d558dca3c23430fd2f93259
George Chaves <george_hfc@yahoo.com.br>
2023-04-26 16:45:19 +0100

    highlight banner

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// document.body.onmousemove = moveCursor;
+
+// var curTxt = document.createElement('div');
+
+// curTxt.id = "cursorText";
+// curTxt.innerHTML = "Hidrate-se! 💦";
+
+// document.body.appendChild(curTxt);
+
+// var curTxtLen = [curTxt.offsetWidth,curTxt.offsetHeight];
+
+// function moveCursor(e){
+//     if(!e) {
+//       e = window.event;
+//     }
+
+//     curTxt.style.left = e.clientX - curTxtLen[0] + 'px';
+//     curTxt.style.top = e.clientY - curTxtLen[1] + 'px';
+// }
