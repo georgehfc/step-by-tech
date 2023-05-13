4456783404ff9ec828f398a31e9c1ce24bf74fce
George Chaves <george_hfc@yahoo.com.br>
2023-05-13 09:16:05 +0100

    csv parse

--- js/app.js
+++ js/app.js
@@ -17,3 +17,32 @@
 //     curTxt.style.left = e.clientX - curTxtLen[0] + 'px';
 //     curTxt.style.top = e.clientY - curTxtLen[1] + 'px';
 // }
+
+console.log('Hi! Parsing CSV:');
+async function getData() {
+  const resposta = await fetch('./Bebedouros.csv');
+  const bebedouros = await resposta.text();
+
+  const tabela = bebedouros.split('\n').slice(1);
+
+  tabela.forEach(linha => {
+    const coluna = linha.split(',');
+    const nome = coluna[0];
+    const end = coluna[1];
+    const loc = coluna[2];
+    const src = coluna[3];
+
+    var div = document.createElement("div.item");
+    var image = document.createElement("img");
+    image.src = `${src}`;
+    div.appendChild(image);
+    // div.innerHTML = "Image added";
+
+    console.log(nome);
+
+    const csv = document.getElementById("csv_content");
+    csv.append(div);
+
+  })
+}
+getData();
